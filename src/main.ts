a3e24669050d8732c9e7dbad6f444b87ca2b9fbd
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2025-09-15 13:03:33 +0200

    Fetch and parse demo KPI results in `main.ts`
    - Added logic to fetch and parse a demo KPI results file from a remote source.
    - Integrated `parse` function to process the fetched data.
    - Updated `store` to save parsed results.
    - Adjusted `main.ts` to include error handling for fetch and parse operations.

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,34 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './assets/styles/dashboard-card.scss';
 import router from './router'
+import {parse} from "./util/Parser.ts";
+import {store} from "./store.ts";
 
 declare var __DEMO_MODE__: string;
 
 async function main() {
-    let App;
-    if (__DEMO_MODE__) {
-        console.log('Running in demo mode');
-        App = await import("./App.vue");
-        // App = await import("./App-demo.vue");
-    } else {
-        console.log('Running in default mode');
-        App = await import("./App.vue");
-    }
+
+    const App = await import("./App.vue");
     createApp(App.default).use(router).mount('#app')
+
+
+    try {
+        // Load demo file from public folder
+        const response = await fetch('https://raw.githubusercontent.com/fraunhofer-iem/spha-ui/refs/heads/main/example/kpi-results.json');
+        if (response.ok) {
+
+            const rawData = await response.json();
+            const parsedResult = parse(rawData);
+
+            if (parsedResult) {
+                store.addResult(parsedResult);
+            } else {
+                console.error('Failed to parse demo file data');
+            }
+        }
+    } catch (error) {
+        console.error('Error loading demo file:', error);
+    }
 }
 
 main();
